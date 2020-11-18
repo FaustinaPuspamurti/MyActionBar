ed962774e261aba66b1a15519e63dc7c7be991d8
FaustinaPuspamurti <faustinapuspamurti2105@gmail.com>
2020-11-18 08:30:47 +0700

    MyActionBar

---
+++ app/src/main/java/com/dicoding/picodiploma/myactionbar/MenuActivity.kt
@@ -0,0 +1,16 @@
+package com.dicoding.picodiploma.myactionbar
+
+import android.app.SearchManager
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.widget.SearchView
+import android.widget.Toast
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
